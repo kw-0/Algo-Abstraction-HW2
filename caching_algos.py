f4c2adbbb69b25e677043a51ca70fc84e32a3e8f
Kylen Williams <kylenwilliams99@gmail.com>
2026-03-03 14:58:32 -0500

    adds first two alogirhtsm and input

--- caching_algos.py
+++ caching_algos.py
@@ -1,9 +1,37 @@
 def FIFO(cache_size, requests):
-    return 1
+    cache = []
+    hits = 0
+
+    for request in requests:
+        if request in cache:
+            hits += 1
+        else:
+            if len(cache) < cache_size:
+                cache.append(request)
+            else:
+                cache.pop(0)
+                cache.append(request)
+
+    return hits
 
 
 def LRU(cache_size, requests):
-    return 1
+    cache = []
+    hits = 0
+
+    for request in requests:
+        if request in cache:
+            hits += 1
+            cache.remove(request)
+            cache.append(request)
+        else:
+            if len(cache) < cache_size:
+                cache.append(request)
+            else:
+                cache.pop(0)
+                cache.append(request)
+
+    return hits
 
 
 def OPTFF(cache_size, requests):
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from caching_algos import FIFO, LRU, OPTFF
+from src.caching_algos import FIFO, LRU, OPTFF
 
 if __name__ == "__main__":
-    print(1)
+    capacity, m = input("Cache Capacity and Number of Requests (m): ").split()
+    if capacity == "" or m == "":
+        print("No cache capacity or m provided")
+        capacity = 0
+        m = 0
+    else:
+        capacity = int(capacity)
+        m = int(m)
+
+    requests = list(map(int, input("Requests: ").split()))
+
