5707a06defbb447d477b10ecbf72778ce626534b
Kylen Williams <kylenwilliams99@gmail.com>
2026-03-03 12:54:08 -0500

    adds inital structure

---
+++ caching_algos.py
@@ -0,0 +1,10 @@
+def FIFO(cache_size, requests):
+    return 1
+
+
+def LRU(cache_size, requests):
+    return 1
+
+
+def OPTFF(cache_size, requests):
+    return 1
---
+++ main.py
@@ -0,0 +1,4 @@
+from caching_algos import FIFO, LRU, OPTFF
+
+if __name__ == "__main__":
+    print(1)
