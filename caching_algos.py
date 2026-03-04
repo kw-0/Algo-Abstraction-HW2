b4b5d125e6dc86b8d1e8d0c63f90406759b182d2
Emma Min Baumgartner <146688511+emmabaumgartner@users.noreply.github.com>
2026-03-03 19:16:53 -0500

    add optff, looking into reduced schedules
    uses same logic as prior policies, but additionally creates future request list, future cache, and removes the largest index (furthest) element.

--- caching_algos.py
+++ caching_algos.py
@@ -34,5 +34,25 @@ def LRU(cache_size, requests):
     return hits
 
 
+
 def OPTFF(cache_size, requests):
-    return 1
+    cache = []
+    hits = 0
+
+    for request in requests:
+        if request in cache:
+            hits += 1
+        else:
+            if len(cache) < cache_size:
+                cache.append(request)
+            else:
+                future_requests = requests[requests.index(request) + 1:]
+                future_cache = [item for item in cache if item in future_requests]
+                # remove furthest in future request
+                if future_cache:
+                    to_remove = max(future_cache, key=lambda x: future_requests.index(x))
+                    cache.remove(to_remove)
+                else:
+                    cache.pop(0)
+                cache.append(request)
+    return hits
