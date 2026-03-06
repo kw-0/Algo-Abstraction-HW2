d446027755645fd367891362d74680d3412cdde9
Emma Min Baumgartner <146688511+emmabaumgartner@users.noreply.github.com>
2026-03-06 17:40:38 -0500

    random data gen

--- random_data_gen.py
+++ random_data_gen.py
@@ -1,1 +1,18 @@
-print(1)
+# generate random cache and requests
+# run each algo on the random inputs
+# measure the misses
+# first three random example input and output put into example.in and example.out
+
+import random
+
+from caching_algos import FIFO, LRU, OPTFF
+
+def generate_size_and_requests():
+    cache_size = random.randint(1, 10)
+    requests = []
+    for _ in range(random.randint(1, 20)):
+        requests.append(random.randint(1, 20))
+    return cache_size, requests
+
+if __name__ == "__main__":
+    main()
